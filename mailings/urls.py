f07a260faa5b1e3f8160bc4b9101d575a0dcb193
Sergey Korolev <bongrichard@rambler.ru>
2024-06-01 19:31:43 +0300

    Настроен механизм CRUD + html верстка + static

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,18 +1,16 @@
-# from django.urls import path
+from django.urls import path
 
 from mailings.apps import MailingsConfig
 
-# from mailings.views import MailingListView, MailingDetailView, MailingUpdateView, MailingDeleteView, \
-#     MailingCreateView, ContactView
+from mailings.views import MailingListView, MailingUpdateView, MailingDetailView, MailingDeleteView, MailingCreateView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
-    # path('', MailingListView.as_view(), name='home'),
-    # path('contact', ContactView.as_view(), name='contact'),
-    # path('mailing_detail/<int:pk>', MailingDetailView.as_view(), name='mailing_detail'),
-    # path('mailing_update/<int:pk>', MailingUpdateView.as_view(), name='mailing_update'),
-    # path('mailing_delete/<int:pk>', MailingDeleteView.as_view(), name='mailing_delete'),
-    # path('create/', MailingCreateView.as_view(), name='create_mailing')
+    path('', MailingListView.as_view(), name='mailings_list'),
+    path('mailing_update/<int:pk>', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailing_detail/<int:pk>', MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailing_delete/<int:pk>', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('create/', MailingCreateView.as_view(), name='create_mailing')
 
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, UpdateView, DetailView, DeleteView, CreateView
+from mailings.models import Mailing
+from mailings.forms import MailingUpdateForm
 
 # Create your views here.
+class MailingListView(ListView):
+    model = Mailing
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        mailings = Mailing.objects.all()
+        context['mailings_list'] = mailings
+        return context
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    form_class = MailingUpdateForm
+    success_url = reverse_lazy('mailings:mailings_list')
+
+    def get_success_url(self):
+        return reverse('mailings:mailing_detail', args=[self.kwargs.get('pk')])
+
+class MailingDetailView(DetailView):
+    model = Mailing
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    success_url = reverse_lazy('mailings:mailings_list')
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    form_class = MailingUpdateForm
+    success_url = reverse_lazy('mailings:mailings_list')
